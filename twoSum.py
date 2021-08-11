9ba5950572244be2c172b67edf1773588df68975
sr447011 <rayswanagan@gmail.com>
2021-08-11 14:40:14 -0400

    solution added

--- twoSum.py
+++ twoSum.py
@@ -1,1 +1,10 @@
-
+#this is my first leetCode attempt
+class Solution:
+    def twoSum(self, integers: List[int], target: int) -> List[int]:
+        passed = {}
+        for i, num in enumerate(integers):
+            # check if the complement to target of integers[i], had already been in integers
+            if target - num in passed:
+                return [i, passed[target - num]]
+            else:
+                passed[num] = i
