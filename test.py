a983d0303afa36b3e014a365721c1338c305b3b2
sr447011 <rayswanagan@gmail.com>
2021-08-11 23:55:26 -0400

    final final

---
+++ test.py
@@ -0,0 +1,5 @@
+import unittest
+import twoSum.py
+
+stuff = [2,7,11,15]
+twoSum(self, stuff, 9)
--- twoSum.py
+++ twoSum.py
@@ -1,16 +1,16 @@
 # O(n^2) time, O(1) space
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+#class Solution:
+#    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#            for j in range(i+1, len(nums)):
+#                if nums[i] + nums[j] == target:
+#                    return [i, j]
 
 #O(n) time, O(n) space We can use a hash table to store the numbers as keys and
 #the indices as values. Then we check to see if the difference (target - number)
 #is in the hash table. If it is, return both the index of the number and the
 #index of the difference.
 class Solution:
+    List = [2,7,11,15]
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         values = {}
         for i, num in enumerate(nums):
@@ -24,15 +24,15 @@ class Solution:
 #array and creating an array of the numbers and their indices, create two
 #pointers i and j where i = 0 and j = len(array)-1. If the sum is smaller than
 #target, increment i, if larger, increment j. If equal, return the indices.
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(nums) - 1
-        sortedNums = sorted(zip(nums, range(len(nums))))
-        while i < j:
-            if sortedNums[i][0] + sortedNums[j][0] == target:
-                return [sortedNums[i][1], sortedNums[j][1]]
-            elif sortedNums[i][0] + sortedNums[j][0] < target:
-                i += 1
-            elif sortedNums[i][0] + sortedNums[j][0] > target:
-                j -= 1
+#class Solution:
+#    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#        i = 0
+#        j = len(nums) - 1
+#        sortedNums = sorted(zip(nums, range(len(nums))))
+#        while i < j:
+#            if sortedNums[i][0] + sortedNums[j][0] == target:
+#                return [sortedNums[i][1], sortedNums[j][1]]
+#            elif sortedNums[i][0] + sortedNums[j][0] < target:
+#                i += 1
+#            elif sortedNums[i][0] + sortedNums[j][0] > target:
+#                j -= 1
