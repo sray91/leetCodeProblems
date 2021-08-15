92749453bed7b4bce85bcfc28e2576cc2948e8bb
sr447011 <rayswanagan@gmail.com>
2021-08-14 21:12:54 -0400

    leetCode day 4

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,13 +1,29 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        List = [character for character in s]
-        result = 0
-        for i in range(len(List)):
-            if List[i] == 'I': result+=1
-            elif List[i] == 'V': result+=5
-            elif List[i] == 'X': result+=10
-            elif List[i] == 'L': result+=50
-            elif List[i] == 'C': result+=100
-            elif List[i] == 'D': result+=500
-            elif List[i] == 'M': result+=1000
+        # make a hashmap for lookup
+        map = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        # now we want to move backwards to determine if the higher number is ahead
+        # N returns an integer the length of string s
+        # i is one less than N because the while loop goes to 0
+        N = len(s) # len('MCMXCIV') = 7, i = 7 -1 = 6
+        i = N-1
+        result = 0 #initialize the result
+
+        while i >= 0:
+            # if the i value is less than the i+1 value, subtract the i values
+            # example IV -> I = 1, V = 5, IV = 5-1 = 4
+            # use "i < N-1" because the first iteration will filter out
+            if i < N-1 and map[s[i]] < map[s[i+1]]:
+                result -= map[s[i]]
+            else:
+                result += map[s[i]]
+            i-=1
         return(result)
--- test.py
+++ test.py
@@ -1,34 +1,5 @@
 # Import required module
 import time
-import RomantoInteger.py
-
-
-# define function to implement for loop
-def for_loop(n):
-    result = []
-    for i in range(n):
-        result.append(i**2)
-    return result
-
-
-# define function to implement list comprehension
-def list_comprehension(n):
-    return [i**2 for i in range(n)]
-
-# function to test
-def reverse(x: int) -> int:
-    conv_str = str(x) #convert int to string and assign to strg
-    if x >= 0:
-        revstr = conv_str[::-1] # this reverses the string
-    else:
-        #this strips off the sign which is at iterable 0 by starting to count at 1
-        tempstr = conv_str[1:]
-        # then do the same operation as in line 21
-        tempstr2 = tempstr[::-1]
-        # this adds the negative sign back in while its a string
-        revstr = "-" + tempstr2
-    if int(revstr) > (2**31) or int(revstr) < ((2**31)*(-1)): return 0
-    else: return int(revstr)
 
 # Driver Code
 
@@ -50,7 +21,7 @@ def reverse(x: int) -> int:
 
 # display current string values
 test_int = 123
-test_string = "III"
+test_string = "MCMXCIV"
 # test_string = reverse(test_int)
 test_result = romanToInt(test_string)
 print(test_result)
