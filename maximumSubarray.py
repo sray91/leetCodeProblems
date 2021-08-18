67c07451494f99ba36bcf9a0208e364e33a3948f
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2021-08-18 15:03:41 -0400

    LeetCode day 8

---
+++ maximumSubarray.py
@@ -0,0 +1,7 @@
+# Maximum Subarray
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# A subarray is a contiguous part of an array.
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+      
+        
