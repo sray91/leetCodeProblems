129f4e39c181f540be9d8cda47a3578b86576386
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2022-02-07 14:20:05 -0500

    Create countChars.java
    BenefitFocus programming challenge

---
+++ countChars.java
@@ -0,0 +1,33 @@
+class countChars {
+// iterative solution
+void countFre(string S)
+    {
+        for(char c = ‘a’;c <= ‘z’;++c)
+        {
+            int frequency = 0;
+            for(int i = 0;i < S.length();++i)
+                if(S[i] == c)
+                    frequency++;
+            cout << c << ‘ ‘ << frequency << endl;
+        }
+    }
+
+// hash table solution
+ int Frequency[26];
+
+    int hashFunc(char c)
+    {
+        return (c - ‘a’);
+    }
+
+    void countFre(string S)
+    {
+        for(int i = 0;i < S.length();++i)
+        {
+            int index = hashFunc(S[i]);
+            Frequency[index]++;
+        }
+        for(int i = 0;i < 26;++i)
+            cout << (char)(i+’a’) << ‘ ‘ << Frequency[i] << endl;
+    }
+}
